3f98ee3d452cdd038b225c126b9f3cd1966ebce2
Alejandre123412 <aangbla2006@gmail.com>
2026-01-26 12:16:07 +0100

    Everithing Working except Camera and PLayer Position

--- assets/scenes/title/TitleScreen.cs
+++ assets/scenes/title/TitleScreen.cs
@@ -1,13 +1,12 @@
 using Godot;
-using AnimaParty.assets.script.data;
 using AnimaParty.assets.script.types;
-//using AnimaParty.assets.script.utils;
+using AnimaParty.autoload;
 
 namespace AnimaParty.assets.scenes.title;
 public partial class TitleScreen : Node
 {
-	private bool isPress;
-	[Export] private PackedScene nextScene;
+	private bool _isPress;
+	[Export] private PackedScene _nextScene;
 	public override void _Ready()
 	{
 		//var mods=ModLoader.LoadAllMods().GetLoadedMods();
@@ -18,22 +17,29 @@ public partial class TitleScreen : Node
 	{
 		if (!Player.IsAcceptableDevice(@event)) return;
 		bool hasPress = @event.IsActionPressed("ui_l1")||@event.IsActionPressed("ui_r1");
-		if (!isPress && hasPress)
+		if (!_isPress && hasPress)
 		{
-			isPress = true;
+			_isPress = true;
 		}
 
-		if (isPress && hasPress)
+		if (_isPress && hasPress)
 		{
+			if(_nextScene == null) return;
 			GD.Print("Pressed");
-			//GD.Print($"Diviced encharge: {@event.Device}");
-			PlayerData.AddDevice(@event.Device);
+			GD.Print($"Diviced encharge: {@event.Device}");
+			PlayerData.Instance.AddDevice(@event.Device);
 			Cambiar();
 		}
 	}
 
 	private void Cambiar()
 	{
-		GetTree().ChangeSceneToFile(nextScene.GetPath());
+		if (_nextScene == null)
+		{
+			GD.PrintErr("Error no existe la siguente escena");
+			return;
+		}
+		GD.Print($"NextScene path: {_nextScene?.ResourcePath}");
+		GetTree().ChangeSceneToPacked(_nextScene);
 	}
 }
